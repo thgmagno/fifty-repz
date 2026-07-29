435d20a70f771a62aa1615d5a4dafb09700abc4f
Magno <thgmgn@gmail.com>
2026-07-29 11:22:53 -0300

    add AppSidebar component to private layout

---
+++ app/(private-route)/layout.tsx
@@ -0,0 +1,16 @@
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main className="flex flex-col flex-1">
+        <SidebarTrigger />
+        <section className="px-2 pb-10 flex-1">
+            {children}
+        </section>
+      </main>
+    </SidebarProvider>
+  )
+}
